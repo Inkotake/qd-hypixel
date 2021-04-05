2a5a1eeb57eefa06a673784d6fde141c946b26ba
a6513375 <52791712+a6513375@users.noreply.github.com>
2021-04-05 23:31:23 +0800

    Update main.py

--- GetCsrfTkoen/main.py
+++ GetCsrfTkoen/main.py
@@ -1,18 +1,16 @@
 import requests
 import re
 import sys
-
-from requests.api import get
-
-
-url = input('url plz\n>>>')
+import json
+import warnings
+url = input('Please input hypixel url\n>>>')
 
 
 def get_chaim(url: str,):
-    if not 'hypixel' in url:
+    if not 'hypixel.net/claim-reward' in url:
         print('Failed: Not a Hypixel url')
         sys.exit()
-    print('Csrf loading... Speed depond on GFW')
+    print('Csrf loading... Speed depend on GFW')
     headers = {
         "referer": "https://rewards.hypixel.net/claim-reward/5d04951a",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
@@ -27,13 +25,23 @@ def get_chaim(url: str,):
         print('Failed: invalid url')
         sys.exit()
 
+    rewardjss = re.findall(r"window.appData \= \'(.*)\';",hyp1_req.text)
+    rewardjs = json.loads(rewardjss[0])
+    for x in rewardjs['rewards']:
+        print (x['reward'])
+
+
+
+    rechoose = input('Please choose a reward (0~2)\n>>>')
     data = {
         "watchedFallback": 'false',
         '_csrf': re1.group().replace("Token = ", '').replace('''"''', ''),
-        'activeAd': '2',
+        'activeAd': rechoose,
         'option': '0',
-        'id': '',
+        'id': rewardjs['id'],
     }
+
+    print('You choose',rechoose,'Now Post reward api...')
 
     # hypurl.replace('https://hypixel.net/claim-reward/','')
     urllclaim = "https://rewards.hypixel.net/claim-reward/claim" + '?' + str(data).replace("{", "").replace(
@@ -48,4 +56,5 @@ def get_chaim(url: str,):
 
 print(get_chaim(url))
 
-#window.appData = '{"rewards":[{"amount":10,"rarity":"COMMON","reward":"dust"},{"package":"specialoccasion_reward_card_skull_pot_o\'_gold","rarity":"COMMON","reward":"housing_package"},{"gameType":"SURVIVAL_GAMES","amount":3000,"rarity":"COMMON","reward":"coins"}],"id":"197d6535","skippable":false,"dailyStreak":{"value":0,"score":0,"highScore":0,"keeps":true,"token":false},"ad":{"video":"TRsCiBNYY7M","duration":28,"link":"https://store.hypixel.net/?utm_source=rewards-video&utm_medium=website&utm_content=TRsCiBNYY7M&utm_campaign=Rewards","buttonText":"Visit the Store"},"activeAd":0,"playwire":false}';
+# window.appData = '{"rewards":[{"intlist":[4],"rarity":"RARE","reward":"mystery_box"},{"amount":1,"rarity":"RARE","reward":"adsense_token"},{"amount":20,"rarity":"RARE","reward":"dust"}],"id":"3f45f1e0","skippable":false,"dailyStreak":{"value":0,"score":0,"highScore":0,"keeps":true,"token":false},"ad":{"video":"GNhbZ5guqO0","duration":30,"link":"https://store.hypixel.net/?utm_source=rewards-video&utm_medium=website&utm_content=GNhbZ5guqO0&utm_campaign=Rewards","buttonText":"Visit the Store"},"activeAd":2,"playwire":false}';
+# window.appData = '{"rewards":[{"amount":10,"rarity":"COMMON","reward":"dust"},{"package":"specialoccasion_reward_card_skull_pot_o\'_gold","rarity":"COMMON","reward":"housing_package"},{"gameType":"SURVIVAL_GAMES","amount":3000,"rarity":"COMMON","reward":"coins"}],"id":"197d6535","skippable":false,"dailyStreak":{"value":0,"score":0,"highScore":0,"keeps":true,"token":false},"ad":{"video":"TRsCiBNYY7M","duration":28,"link":"https://store.hypixel.net/?utm_source=rewards-video&utm_medium=website&utm_content=TRsCiBNYY7M&utm_campaign=Rewards","buttonText":"Visit the Store"},"activeAd":0,"playwire":false}';
